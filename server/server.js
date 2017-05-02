d41bbaccda72741399d02cee0d9d92018e7d7a3b
SP6MEN32 <mixr6@hotmail.fr>
2017-05-02 13:39:10 +0200

    Add express

---
+++ server/server.js
@@ -0,0 +1,16 @@
+//web server
+var express = require('express');
+var app = express();
+ var momentjs = require('moment');
+ //var faker = require('faker');
+ var nodefs = require('fs');
+ var bodyparser = require('body-parser');
+
+app.listen(3000, function(){
+	console.log('server ok');
+});
+
+//route 
+app.post('/customers', function(req, res){
+res.send('hello');
+});
