1e0728a3b5113e1a4352c42de3dfb84351fea48f
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-05-02 16:58:53 +0200

    Verification of completed inputs

--- client/partie_clients/js/app.js
+++ client/partie_clients/js/app.js
@@ -1,1 +1,142 @@
-console.log("Hello");
+function surligne(champ, erreur){
+   
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+
+function verifGender(champ){
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifFirstName(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+function verifName(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+
+
+function verifCity(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+function verifAdress(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+function verifBirthdate(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+function verifZipCode(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+function verifPhoneNumber(champ) {
+
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }	
+}
+
+
+
+
+
+
+
+// function verifForm(f){
+   
+//    var genderOk = verifGender(f.gender);
+//    var firstNameOk = verifMail(f.firstName);
+//    // var ageOk = verifAge(f.age);
+   
+//    if(genderOk && firstNameOk)
+//       return true;
+//    else
+//    {
+//       alert("Veuillez remplir correctement tous les champs");
+//       return false;
+//    }
+// }
