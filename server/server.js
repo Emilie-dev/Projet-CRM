f3b420d57f104e288cfe1702883637d7d5fbcd3c
marco32 <marcofrosi32@gmail.com>
2017-05-02 17:17:52 +0200

    Add function Add/Update/Delete/Get customers

--- server/server.js
+++ server/server.js
@@ -50,21 +50,28 @@ res.send(fakeCustomers);
 
 //route post-produits
 app.post('/products', function(req, res){
+});
+
+
 
 app.post('/customers', function(req, res){
-	res.send('hello');
+	AddData(dbCustomers, req);
 });
 
 //routeGetClients
 app.get('/customer/getAll', function(req, res){
-	res.send('/getClient');
-
+	fs.readFile('customers.json',function read(err,data){
+	 		 	if(err) throw err;
+	 		 	data=data;
+		 res.send(data);
+	 })
 });
 
-app.post('/customers/update', function(){
-	res.send('/customers/update');
+app.post('/customers/update', function(req, res){
+	var add= req.body.db;
+	UpdateData(dbCustomers, add);
+	
 });
-
 //route delete/suppr clients
 app.post('/customers/delete', function(req, res){
 	res.send('/customers/delete');
@@ -103,7 +110,39 @@ app.post('/orders/update', function(){
 	res.send('/orders/update');
 });
 
+// function Customers
+// fs.readFile sert a parcourir le fichier contenant la base client,
+// fs.writeFile sert a réecrire le fichier.
+function AddData(dir,req){
+	var data = req.body;
+	var addCustomer= {
+		"gender" : data.gender,
+	 	"name" : data.name,
+		"firstName" : data.firstName,
+		"birthdate" : data.birthdate,
+		"city": data.city,
+		"zipCode": data.zipCode,
+		"address" : data.address,
+		"phoneNumber" : data.phoneNumber,
+		"registrationDate" : now.format('MMMM Do YYYY'),
+	 	};
+	 nodefs.readFile(dir,function(err,data){
+	 	obj= JSON.parse(data)
+	 	if(err)throw err;		
+	 obj.push(addCustomer)
+		json=JSON.stringify(obj)
+	 nodefs.writeFile(dir,json, function(err){
+	 	if(err) throw err;
+	 });
+	 })	
+	}
 
+function UpdateData(dir, add){
+ 	 nodefs.writeFile(dir,add, function(err){
+	  	if(err) throw err;
+	 }); 	
+	
+	}
 
 
 
