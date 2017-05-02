c5c45df25397702b6cf9c7e015dcae57266824f7
dinno1986 <dimitri.castell@hotmail.fr>
2017-05-02 15:02:23 +0200

    rajout des routes delete et post produits et commande

--- server/server.js
+++ server/server.js
@@ -1,16 +1,38 @@
 //web server
 var express = require('express');
 var app = express();
- var momentjs = require('moment');
+var momentjs = require('moment');
  //var faker = require('faker');
- var nodefs = require('fs');
- var bodyparser = require('body-parser');
+var nodefs = require('fs');
+var bodyparser = require('body-parser');
 
 app.listen(3000, function(){
 	console.log('server ok');
 });
 
+//base de données
+var TableClient = {
+
+}
+
+
 //route 
 app.post('/customers', function(req, res){
 res.send('hello');
+});
+
+//route post-produits
+app.post('/products', function(req, res){
+});
+
+//route post-commande
+app.post('/orders', function(req, res){
+});
+
+//route delete/suppr clients
+app.post('/customers/delete', function(req, res){
+});
+
+// route delete/suppr produits
+app.post('/products/delete', function(req, res){
 });
