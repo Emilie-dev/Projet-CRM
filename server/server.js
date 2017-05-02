c65e2aa9bdadd97a8b095d55ee5d90af8ef6d335
dubech <franck.dubech@hotmail.com>
2017-05-02 14:54:41 +0200

    Ajout des route update

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 //web server
 var express = require('express');
 var app = express();
- var momentjs = require('moment');
- //var faker = require('faker');
- var nodefs = require('fs');
- var bodyparser = require('body-parser');
+var momentjs = require('moment');
+var faker = require('faker');
+var nodefs = require('fs');
+var bodyparser = require('body-parser');
 
 app.listen(3000, function(){
 	console.log('server ok');
 });
 
 //route 
-app.post('/customers', function(req, res){
+app.post('/customers', function(req, res)){
 res.send('hello');
 });
+
+app.post('/customers/update', function()){
+
+}
+
+app.post('/orders/update', function()){
+
+}
+
+app.post('/products/update', function()){
+	
+}
