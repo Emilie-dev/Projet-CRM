f6dc8d989a9ba4eac30a58a123f83bc36eacaf7b
Grégory <claudejurado@orange.fr>
2017-05-03 10:36:28 +0200

    reception des données clients en get et preparation de la fonction affichage

--- client/partie_clients/js/app.js
+++ client/partie_clients/js/app.js
@@ -3,3 +3,28 @@ console.log("Hello");
 $(function(){
 	$("#customerTable").tablesorter();
 });
+
+var customers=[];
+
+function recept(){
+	$.ajax({
+		url:"http://192.168.1.152/customers/",
+		data: {
+		task: "get",
+		key: "customers",
+		},
+		success : function(data){
+		console.log(data)
+		}
+	})
+	.done(function(data){
+		customers=JSON.parse(data);
+		console.log(customers);
+		load(customers);				
+	})
+};
+
+function load(tab){
+	for (i=0;i<tab.length;i++){
+		
+};
