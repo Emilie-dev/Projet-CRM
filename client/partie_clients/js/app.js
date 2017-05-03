a04bdcea757de15a26091fedf947ffa01244c328
Grégory <claudejurado@orange.fr>
2017-05-03 13:50:29 +0200

    modification des chemins

--- client/partie_clients/js/app.js
+++ client/partie_clients/js/app.js
@@ -1,172 +1,15 @@
-
-function surligne(champ, erreur){
-
-   if(erreur)
-      champ.style.backgroundColor = "#fba";
-   else
-      champ.style.backgroundColor = "";
-}
-
-function verifGender(champ){
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }
-}
-
-
-function verifFirstName(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-function verifName(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-
-
-function verifCity(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-function verifAdress(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-function verifBirthdate(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-function verifZipCode(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-   }  
-}
-
-function verifPhoneNumber(champ) {
-
-   if(champ.value.length < 2 || champ.value.length > 25)
-   {
-      surligne(champ, true);
-      return false;
-   }
-   else
-   {
-      surligne(champ, false);
-      return true;
-
-   }  
-}
-
-function verifForm(f){
-
-   var genderOk = verifGender(f.gender);
-   var firstNameOk = verifFirstName(f.firstName);
-   var nameOk = verifName(f.name);
-   var cityOk = verifCity(f.city);
-   var addressOk = verifAdress(f.address);
-   var birthDateOk = verifBirthdate(f.birthdate);
-   var zipCodeOk = verifZipCode(f.zipCode);
-   var phoneNumberOk = verifPhoneNumber(f.phoneNumber);
-
-   if(genderOk && firstNameOk && nameOk && cityOk && addressOk && birthdateOk && zipCodeOk)
-      return true;
-   else
-   {
-      alert("Veuillez remplir correctement tous les champs");
-      return false;
-   }
-}
-
-$("submit").on( 'click',function (event) {
-   event.preventDefault();
-   verifForm()
-   if(true){
-      alert("Votre client viens d'être enregistrer dans la base de données"),
-      $('form input').val("");
-   } alert('Verifier le formulaire')
-
-})
+console.log("Hello");
 
 $(function(){
-  $("#customerTable").tablesorter();
+	$("#customerTable").tablesorter();
 });
-
 
 var customers=[];
 
 function recept(){
 	$.ajax({
-		url:"http://192.168.1.152/customers/",
-		data: {
-		task: "get",
-		key: "customers",
-		},
+		url:"http://192.168.1.152/customers/getAll",
+		
 		success : function(data){
 		console.log(data)
 		}
@@ -180,7 +23,5 @@ function recept(){
 
 function load(tab){
 	for (i=0;i<tab.length;i++){
-		$("tbody").append("<tr></tr>")
-		$("tr").append("")
-		}		
+		
 };
